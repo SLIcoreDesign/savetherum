d1de25bfdebecf6c5cdfc97cad8ace75913f91b7
James Higgins <jhiggins@slicore.com>
2018-07-14 19:22:05 -0600

    Initial Commit

---
+++ SaveTheRum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheRum
+//
+//  Created by James Higgins on 2018-07-14.
+//  Copyright © 2018 SLIcore Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
